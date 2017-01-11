81b05b0bb2bc07778695365b5182f4db3cfe2a89
harish120896 <saiharish120896@gmail.com>
2017-01-11 23:31:38 +0530

    done

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ console.log("starting app");
 const fs = require('fs');
 const os = require('os');
 
+var command = process.argv[2];
+
+if(command === 'add'){
+    console.log("Added new note");
+}else if(command === 'list'){
+    console.log("list of notes are as follows");
+}else if(command === 'read'){
+ console.log("reading note");   
+}else if(command === 'remove'){
+    console.log("removing note");
+}else{
+    console.log("Check your command");
+}
+console.log(process.argv[3]);
 
